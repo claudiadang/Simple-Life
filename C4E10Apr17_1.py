e4037fd0b98278ff900921f195bbbadf2ba03c6e
claudiadang <claudiadangapps@gmail.com>
2017-04-21 19:07:43 +0700

    track

---
+++ C4E10Apr17_1.py
@@ -0,0 +1,20 @@
+print ( "I am Claudia")
+print (8)
+print (3)
+a=2
+print(a)
+b=5
+##print(b)
+##n = input( "what's your name?")
+##print("Hello",n)
+####print("a" +"b")
+##a = input ("what's your age?")
+
+yob= int(input ("Your year of birth?"))
+
+print(2017 -yob)
+
+
+
+
+
